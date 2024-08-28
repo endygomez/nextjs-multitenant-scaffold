81e73d5bddabeabfad445ea7ed689d1dddcfcbf6
Endy Gómez <egomez@MacBook-Pro-de-Endy.local>
2024-08-28 15:41:50 -0500

    Change NextAuth interface

--- nextauth.d.ts
+++ nextauth.d.ts
@@ -1,14 +1,17 @@
 import NextAuth, { DefaultSession } from "next-auth";
 
+type Role = "admin" | "user";
+
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      name: string;
-      email: string;
       tenantId: string;
-      emailVerified?: boolean;
-      role: string;
+      firstName: string;
+      lastName: string;
+      email: string;
+      emailVerified?: Date;
+      role?: Role;
       image?: string;
       token?: string;
     } & DefaultSession["user"];
